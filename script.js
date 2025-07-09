ee1334a2b0a8f2c172e79aa84ce9e7e9ce0e4b9c
Fareedcbr <mdfareedcbr@gmail.com>
2025-07-09 22:06:28 +0530

    update

--- script.js
+++ script.js
@@ -61,3 +61,12 @@ if (profileImg && modal && modalImg && closeModal) {
     }
   });
 }
+
+// Mobile navbar toggle
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelector('.nav-links');
+if (navToggle && navLinks) {
+  navToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('open');
+  });
+}
